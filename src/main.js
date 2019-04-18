338fd0071b75ba2b821c9e7483a3549884f7d63e
dhwani-lanetteam <dhwanij2@gmail.com>
2019-04-18 17:22:08 +0530

    Routing added

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+// a separate routing file
 import router from './router'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,21 @@
 import Vue from 'vue'
+// needed to add rooutes
 import Router from 'vue-router'
+// various components
 import HelloWorld from '@/components/HelloWorld'
 import SingleTemplate from "../components/SingleTemplate";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
-    /*{
+    {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    },*/
+    },
     {
-      path: '/',
+      path: '/single',
       name: 'SingleTemplate',
       component: SingleTemplate
     },
