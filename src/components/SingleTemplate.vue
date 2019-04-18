d73c1018b410835e80f45237e5ca7560894a35fd
dhwani-lanetteam <dhwanij2@gmail.com>
2019-04-18 17:10:24 +0530

    Text hide show using @click v-show

---
+++ src/components/SingleTemplate.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <!-- Click to invoke toggle method which toggles the boolean flag -->
+        <button class="btn btn-dark" @click="toggle">Click me</button>
+        <div class="row" v-show="isShown">
+          <div class="col-md-12 mt-5">
+            <!-- Show if 'isShown' is true -->
+            <p id="text">Hello, {{ user }} </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'SingleTemplate',
+    data() {
+      return {
+        user: 'Dhwani',
+        isShown: false, // Control property
+      };
+    },
+    methods: {
+      toggle() {
+        // Toggles control property
+        this.isShown = !this.isShown;
+      },
+    },
+  }
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import SingleTemplate from "../components/SingleTemplate";
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
+    /*{
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    }
+    },*/
+    {
+      path: '/',
+      name: 'SingleTemplate',
+      component: SingleTemplate
+    },
+
   ]
 })
