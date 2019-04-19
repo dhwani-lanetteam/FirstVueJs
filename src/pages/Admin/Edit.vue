19e4e42ad920f5c08dd285fe7e1bd07a3faaa8ad
dhwani-lanetteam <dhwanij2@gmail.com>
2019-04-19 14:11:20 +0530

    Fetch query param from route

--- src/pages/Admin/Edit.vue
+++ src/pages/Admin/Edit.vue
@@ -1,5 +1,10 @@
 <template>
-  <p>Edit works</p>
+  <div>
+    <div class="title">
+      <!-- Fetch query param from route -->
+      <h1>This is Admin/Edit/{{$route.params.id}}</h1>
+    </div>
+  </div>
 </template>
 
 <script>
