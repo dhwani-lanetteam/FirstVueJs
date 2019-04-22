ecc61ac6abc1c15abb89f6f4a7629857ea07d491
dhwani-lanetteam <dhwanij2@gmail.com>
2019-04-22 17:27:20 +0530

    forms fields are validated on submit button click

--- src/main.js
+++ src/main.js
@@ -4,11 +4,20 @@ import Vue from 'vue'
 import App from './App'
 // a separate routing file
 import router from './router'
+// bootsrap CSS and components
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+// validation library
+import VeeValidate from 'vee-validate';
 
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
+Vue.use(VeeValidate, {
+  // This is the default
+  inject: true,
+  // Important to name this something other than 'fields'
+  fieldsBagName: 'veeFields'
+});
 
 Vue.config.productionTip = false
 
--- src/pages/Login/Registration.vue
+++ src/pages/Login/Registration.vue
@@ -8,71 +8,99 @@
     </div>
     <div class="row">
       <div class="mt-3 col-md-12">
-        <b-form>
+        <b-form @submit.prevent="validateBeforeSubmit">
           <b-form-group id="ig-firstname"
                         label="First Name:">
             <b-form-input id="firstname"
-                          v-model="form.firstname"
+                          name="firstname"
                           type="text"
-                          placeholder="Enter first name">
+                          placeholder="Enter first name"
+                          :class="{'error': errors.has('firstname') }"
+                          v-model="form.firstname"
+                          v-validate="'required'">
             </b-form-input>
+            <span class="error-message">{{ errors.first('firstname') }}</span>
           </b-form-group>
           
           <b-form-group id="ig-lastname"
                         label="Last Name:">
             <b-form-input id="lastname"
-                          v-model="form.lastname"
+                          name="lastname"
                           type="text"
                           placeholder="Enter last name"
+                          :class="{'error': errors.has('lastname') }"
+                          v-model="form.lastname"
+                          v-validate="'required'"
             ></b-form-input>
+            <span class="error-message">{{ errors.first('lastname') }}</span>
           </b-form-group>
           
           <b-form-group id="ig-email"
                         label="Email:">
             <b-form-input id="email"
-                          v-model="form.email"
-                          type="text"
+                          name="email"
+                          type="email"
                           placeholder="Enter email"
-            ></b-form-input>
+                          :class="{'error': errors.has('email') }"
+                          v-model="form.email"
+                          v-validate="'required|email'">
+            </b-form-input>
+            <span class="error-message">{{ errors.first('email') }}</span>
           </b-form-group>
           
           <b-form-group id="ig-password"
-                        label="Last Name:">
+                        label="Password:">
             <b-form-input id="password"
-                          v-model="form.password"
+                          name="password"
                           type="text"
                           placeholder="Enter password"
-            ></b-form-input>
+                          :class="{'error': errors.has('password') }"
+                          v-model="form.password"
+                          v-validate="'required'">
+            </b-form-input>
+            <span class="error-message">{{ errors.first('password') }}</span>
           </b-form-group>
           
           <b-form-group id="ig-fav-food"
                         label="Food:"
                         label-for="input-3">
             <b-form-select id="fav-food"
-                           :options="fruits"
-                           v-model="form.favourite_food">
+                           name="fav-food"
+                           v-model="form.favourite_food"
+                           v-validate="'required'"
+                           :class="{'error': errors.has('fav-food') }"
+                           :options="fruits">
             </b-form-select>
+            <span class="error-message">{{ errors.first('fav-food') }}</span>
           </b-form-group>
           
           <b-form-group id="ig-hobby"
                         label-for="hobby"
                         label="Hobby:">
-            <b-form-checkbox-group id="hobby">
+            <b-form-checkbox-group id="hobby"
+                                   name="hobby"
+                                   v-validate="'required'"
+                                   :class="{'error': errors.has('hobby') }">
               <b-form-checkbox value="sports">Sports</b-form-checkbox>
               <b-form-checkbox value="art">Art</b-form-checkbox>
               <b-form-checkbox value="entertainment">Entertainment</b-form-checkbox>
               <b-form-checkbox value="house_decor">House decor</b-form-checkbox>
             </b-form-checkbox-group>
+            <span class="error-message">{{ errors.first('hobby') }}</span>
           </b-form-group>
           
           <b-form-group id="ig-gender"
                         label-for="gender"
                         label="Gender:">
-            <b-form-radio-group id="gender">
+            <b-form-radio-group id="gender"
+                                name="gender"
+                                v-validate="'required'"
+                                :class="{'error': errors.has('gender') }">
               <b-form-radio value="male">Male</b-form-radio>
               <b-form-radio value="female">Female</b-form-radio>
               <b-form-radio value="other">Other</b-form-radio>
             </b-form-radio-group>
+            <span class="error-message">{{ errors.first('gender') }}</span>
           </b-form-group>
           
           <b-button type="submit" variant="primary">Submit</b-button>
@@ -99,6 +127,19 @@
         },
         fruits: [{ text: 'Select Your Favourite Food', value: null }, 'grapes', 'strawberry', 'apple', 'banana', 'orange'],
       }
+    },
+    methods: {
+      validateBeforeSubmit() {
+        this.$validator.validateAll().then((result) => {
+          // Each field is valid then
+          if (result) {
+            // eslint-disable-next-line
+            alert('Form Submitted!');
+            return;
+          }
+          // One of the fields is not valid then
+        });
+      },
     },
   }
 </script>
